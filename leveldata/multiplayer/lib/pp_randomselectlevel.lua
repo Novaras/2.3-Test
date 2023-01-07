b89022ef00a6e4f6a38c83d4ba13a9e849a336ab
Novaras <tfear091@gmail.com>
2023-01-07 21:16:40 +0000

    fix if no N-size map; refactor a little

--- leveldata/multiplayer/lib/pp_randomselectlevel.lua
+++ leveldata/multiplayer/lib/pp_randomselectlevel.lua
@@ -1,6 +1,6 @@
 dofilepath("data:scripts/lib/table_util.lua");
 
----@return string|nil
+---@return table|nil
 PP_RandomLevel = PP_RandomLevel or function (player_count, excluded_groups)
     player_count = player_count or 2;
     excluded_groups = excluded_groups or {};
@@ -10,13 +10,17 @@ PP_RandomLevel = PP_RandomLevel or function (player_count, excluded_groups)
     local gamemodes = tbl_pack(tbl_filter({ 'dm_hw1', 'dm_hw2', 'Ace_Maps' }, function (groupname)
         return tbl_includesValue(%excluded_groups, groupname) == nil;
     end));
-    local group_idx = 1;
+    local gamemode_idx = 1;
     if (tbl_length(gamemodes) > 1) then
-        group_idx = random(1, tbl_length(gamemodes));
+        gamemode_idx = random(1, tbl_length(gamemodes));
     end
     --- Can be nil in the case randomN is chosen but there are no N sized maps
     ---@type string|nil
-    local gamemode = gamemodes[group_idx];
+    local gamemode = gamemodes[gamemode_idx];
+
+    if (gamemode == nil) then
+        return nil;
+    end
 
     dofilepath("data:leveldata/multiplayer/" .. gamemode .. ".levels");
 
@@ -40,12 +44,30 @@ PP_RandomLevel = PP_RandomLevel or function (player_count, excluded_groups)
             local level = levels[index];
 
             local path = Root .. "\\" .. level.Name .. ".level";
-            dofilepath(path);
+            print("random map: " .. path);
 
-            return path;
+            level.path = path;
+
+            return level;
         end
     end
 
     tbl_push(excluded_groups, gamemode);
     return PP_RandomLevel(player_count, excluded_groups);
+end
+
+function PP_ImportRandomLevel(player_count)
+    local level = nil;
+    -- if we try picking random for N players and there are no N sized maps, we will try picking N+1 until 8
+    for i = player_count, 8 do
+        level = PP_RandomLevel(i);
+        if (level) then
+            break;
+        end
+    end
+
+    dofilepath(level.path);
+    -- we need to re-overwrite the level desc since the imported level set it
+    dofilepath("data:leveldata/multiplayer/dm_pp.levels"); -- so we can look up the level desc
+    levelDesc = LevelList[player_count - 1].Desc;
 end
