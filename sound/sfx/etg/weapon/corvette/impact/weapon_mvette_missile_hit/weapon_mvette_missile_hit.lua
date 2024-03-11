1c88da09202a68e3107e7e159d9e09be3ce91810
Novaras <tfear091@gmail.com>
2024-03-11 16:00:19 +0000

    mvette: create new missiles, weaponfires, and sfx for the missiles fired by mvettes (major volume reduction)

---
+++ sound/sfx/etg/weapon/corvette/impact/weapon_mvette_missile_hit/weapon_mvette_missile_hit.lua
@@ -0,0 +1,96 @@
+-- This file is generated automatically by the Relic Audio Tool
+
+version = 4
+
+volume = 0.35
+maxPolyphony = 4
+envelope = {
+  {
+    distance = 0.000000,
+    volume = 0.5,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+    }
+  },
+  {
+    distance = 1000.000000,
+    volume = 0.3,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+    }
+  },
+  {
+    distance = 2000.0,
+    volume = 0.2,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              0.810000,
+              0.770000,
+    }
+  },
+  {
+    distance = 4000.0,
+    volume = 0.1,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              0.890000,
+              0.700000,
+              0.690000,
+              0.670000,
+    }
+  },
+  {
+    distance = 5000.0,
+    volume = 0.0,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              0.890000,
+              0.700000,
+              0.690000,
+              0.670000,
+    }
+  },
+}
+container = 1
+playlist = 0
+randContainer = 1
+loopingPlaylist = 0
+silenceBreak = 0.000000
+silenceRandom = 0.000000
+randSampContainer = 1
+
